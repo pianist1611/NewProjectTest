9dd84acc4d4956423363bb238e3b61a325d1776d
pianist1611 <pianist161@mail.ru>
2022-12-26 12:01:30 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{VozvedenieNumber(2, 5)}");
+            Console.ReadLine();
+        }
+        static int VozvedenieNumber(int number, int stepen)
+        {
+            int localnumber = number;
+            for(int i = 0; i < stepen; i++)
+            {
+                number *= localnumber;
+            }
+            return number;
+        } 
+    }
+}
